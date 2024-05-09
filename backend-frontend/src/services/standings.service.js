c2509c01d0214ce30de06ee3496653f45abc333f
Jerome Vonk <lelovv@gmail.com>
2024-05-09 15:28:15 -0300

    first commit

---
+++ backend-frontend/src/services/standings.service.js
@@ -0,0 +1,228 @@
+import { format } from 'date-fns'; 
+
+export const standingsService = {
+  getStandings,
+};
+
+const ITERATE_BY_ROUNDS = [1, 2, 3, 4, 5, 7, 8];
+const ITERATE_BY_TEAM = [6];
+const TEAMS = [
+  "Atlético-GO",
+  "Atlético-MG",
+  "Athletico-PR",
+  "Bahia",
+  "Botafogo",
+  "Bragantino",
+  "Cuiabá",
+  "Criciúma",
+  "Cruzeiro",
+  "Corinthians",
+  "Fortaleza",
+  "Flamengo",
+  "Fluminense",
+  "Grêmio",
+  "Internacional",
+  "Juventude",
+  "Palmeiras",
+  "São Paulo",
+  "Vasco",
+  "Vitória",
+]
+
+function getStandings(matches, option, subOption) {
+  
+  // ---------------------------
+  // Create empty standings
+  // ---------------------------
+  const standings = {};
+
+  for (const team of TEAMS) {
+    standings[team] = emptyStandings()
+  }
+
+  // If there are matches, iterate through
+  if (Object.keys(matches).length !== 0) {
+    if (ITERATE_BY_ROUNDS.includes(option)) {
+      iterateByRounds(standings, matches, option, subOption);
+    } else if (ITERATE_BY_TEAM) {
+      iterateByTeam(standings, matches, option, subOption);
+    }
+  }
+
+  // Convert 
+  return convertStandingsToArray(standings);
+}
+
+// -----------------------------------------------
+// Iterate by round in crescent order
+// -----------------------------------------------
+function iterateByRounds(standings, matches, option, subOption) {
+  const details = {
+    startRound: 1,
+    endRound: 38,
+    calculateHome: true,
+    calculateAway: true,
+  }
+
+  switch (option) {
+    case 1:
+      // No changes
+      break;
+
+    case 2:
+      details.calculateAway = false;
+      break;
+
+    case 3:
+      details.calculateHome = false;
+      break;
+
+    case 4:
+      details.endRound = 19
+      break;
+
+    case 5:
+      details.startRound = 20;
+      details.endRound = 38
+      break;
+
+    case 6:
+      // N/A here
+      break;
+
+    case 7:
+      details.endRound = subOption;
+      break;
+
+    case 8:
+      details.dateLimit = format(subOption, 'yyyy-MM-dd'); 
+      break;
+  }
+
+  for (let i = details.startRound; i <= details.endRound; i++) {
+    const round = matches[i];
+
+    for (const match of round) {
+      calculateMatch(standings, match, details.calculateHome, details.calculateAway, details.dateLimit)
+    }
+  }
+}
+
+// -----------------------------------------------
+// Iterate by team
+// -----------------------------------------------
+function iterateByTeam(standings, matches, _option, subOption) {
+  // Get matches order by date in descending order
+  const sortedMatches = getMatchesInDescendingOrder(matches);
+
+  // Get array of teams
+  const teamNames = Object.keys(standings);
+  
+  // For every team
+  for (const team of teamNames) {
+    let toFind = subOption;
+
+    // Look for last X matches
+    for (let i = 0; i < sortedMatches.length && toFind > 0; i++) {
+      const match = sortedMatches[i];
+      
+      // Did the team play at home, away?
+      if (team === match.homeTeam) {
+        calculateMatch(standings, match, true, false, false);
+        toFind--;
+      } else if(team === match.awayTeam) {
+        calculateMatch(standings, match, false, true, false);
+        toFind--;
+      }
+    }
+  }
+}
+
+// -----------------------------------------------
+// Util
+// -----------------------------------------------
+function convertStandingsToArray(standings) {
+  // Convert 
+  const sorted = [];
+  for (const [key, value] of Object.entries(standings)) {
+    sorted.push({
+      team: key,
+      ...value
+    })
+  }
+
+  return sorted;
+}
+
+function emptyStandings() {
+  return { "points": 0, "pointsLost": 0, "matches": 0, "victories": 0, "draws": 0, "losses": 0, "goalsFor": 0, "goalsAgainst": 0, "goalDifference": 0 }
+}
+
+function calculateMatch(standings, match, calculateHome, calculateAway, dateLimit) {
+  const { homeTeam, awayTeam, homeScore, awayScore, started, date } = match;
+
+  // If the game has started 
+  // AND
+  // if dateLimit defined, it's greather than or equal the date
+  // THEN
+  // consider the match
+  if (started && !(dateLimit && date > dateLimit)) {
+
+    // The home team
+    if (calculateHome) calculateStandings(standings[homeTeam], homeScore, awayScore)
+
+    // The away team
+    if (calculateAway) calculateStandings(standings[awayTeam], awayScore, homeScore)
+  }
+}
+
+function getResults(score, oponentScore) {
+  const results = {
+    points: 0,
+    pointsLost: 0,
+    victory: 0,
+    draw: 0,
+    loss: 0,
+    goalsFor: score,
+    goalsAgainst: oponentScore,
+    goalDifference: score - oponentScore
+  }
+
+  if (score > oponentScore) {
+    results.points = 3
+    results.victory = 1
+  } else if (score == oponentScore) {
+    results.points = 1
+    results.pointsLost = 2
+    results.draw = 1
+
+  } else {
+    results.pointsLost = 3
+    results.loss = 1
+  }
+
+  return results;
+}
+
+function calculateStandings(team, score, oponentScore) {
+  const results = getResults(score, oponentScore)
+  team.matches += 1
+  team.points += results.points
+  team.pointsLost += results.pointsLost
+  team.victories += results.victory
+  team.draws += results.draw
+  team.losses += results.loss
+  team.goalsFor += results.goalsFor
+  team.goalsAgainst += results.goalsAgainst
+  team.goalDifference += results.goalDifference
+}
+
+function getMatchesInDescendingOrder(matches) {
+  // Flatten the matches
+  const matchesArray = Object.values(matches).flat();
+
+  // Sort by date descending
+  return matchesArray
+    .filter(item => item.started)
+    .sort((a, b) => new Date(b.date) - new Date(a.date));
+}
